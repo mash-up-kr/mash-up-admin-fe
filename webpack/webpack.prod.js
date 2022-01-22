08c4cd2c17db3d160ae590408ab13f5547bc8b97
이용재 <dev.yongjaelee@gmail.com>
2022-01-23 07:17:30 +0900

    fix: cleanup-webpack-plugin을 제거한다
    https://webpack.js.org/guides/output-management/#cleaning-up-the-dist-folder
    https://webpack.js.org/configuration/output/#outputclean

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = require('./webpack.base.js');
 const PROJECT_ROOT = path.resolve(__dirname, '..');
@@ -15,6 +14,5 @@ module.exports = merge(baseConfig, {
       template: path.resolve(TEMPLATE_PATH, 'index.html'),
       minify: true,
     }),
-    new CleanWebpackPlugin(),
   ],
 });
