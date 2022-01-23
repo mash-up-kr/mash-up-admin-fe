32d3abc1c0c47a0e9e88e25a291fa3069ca782d1
dididy <dev.yongjaelee@gmail.com>
2022-01-23 10:56:48 +0900

    fix: @emotion/babel-preset-css-prop 대신 emotion-ts-plugin 사용하도록 수정한다

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
+const { createEmotionPlugin } = require('emotion-ts-plugin');
 
 const PROJECT_ROOT = path.resolve(__dirname, '..');
 const DIST_PATH = path.resolve(PROJECT_ROOT, 'dist');
@@ -19,13 +20,35 @@ module.exports = {
         test: /\.tsx?$/,
         use: [
           {
-            loader: 'babel-loader',
-          },
-          {
             loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              getCustomTransformers: () => ({
+                before: [
+                  createEmotionPlugin({
+                    // <------------------- here
+                    sourcemap: true,
+                    autoLabel: true,
+                    labelFormat: '[local]',
+                    // if the jsxFactory is set, should we auto insert the import statement
+                    autoInject: true,
+                    // set for react@17 new jsx runtime
+                    // only effect if `autoInject` is true
+                    // set it in createEmotionPlugin options rather than in `tsconfig.json` will generate more optimized codes:
+                    // import { jsx } from 'react/jsx-runtime' for files not using emotion
+                    // import { jsx } from '@emotion/react/jsx-runtime' for files using emotion
+                    jsxImportSource: '@emotion/react',
+                  }),
+                ],
+              }),
+              compilerOptions: {
+                // set jsx pragma to jsx or alias which is from the @emotion/react package to enable css property in jsx component
+                jsxFactory: 'jsx',
+              },
+            },
+            exclude: /node_modules/,
           },
         ],
-        exclude: /node_modules/,
       },
       {
         test: /\.(png|jpg|gif)$/,
