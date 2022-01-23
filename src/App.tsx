d29df398d1cdd0128ca95a68dfa043e1d8b133bc
dididy <dev.yongjaelee@gmail.com>
2022-01-24 00:09:54 +0900

    env: reset css 를 붙이고 Global style을 추가한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Test from '@components/Test';
+import { Global } from '@emotion/react';
+import resetCss from './utils/resetCss';
 
 const App = () => (
   <div>
+    <Global styles={resetCss} />
     <Test />
     world!
   </div>
