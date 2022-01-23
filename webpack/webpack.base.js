72042fb74541d134e0452901ed8f042edbc79e5f
dididy <dev.yongjaelee@gmail.com>
2022-01-24 00:31:31 +0900

    fix: webpack ts-loader에 exclude를 제거한다

--- webpack/webpack.base.js
+++ webpack/webpack.base.js
@@ -18,37 +18,34 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: [
-          {
-            loader: 'ts-loader',
-            options: {
-              transpileOnly: true,
-              getCustomTransformers: () => ({
-                before: [
-                  createEmotionPlugin({
-                    // <------------------- here
-                    sourcemap: true,
-                    autoLabel: true,
-                    labelFormat: '[local]',
-                    // if the jsxFactory is set, should we auto insert the import statement
-                    autoInject: true,
-                    // set for react@17 new jsx runtime
-                    // only effect if `autoInject` is true
-                    // set it in createEmotionPlugin options rather than in `tsconfig.json` will generate more optimized codes:
-                    // import { jsx } from 'react/jsx-runtime' for files not using emotion
-                    // import { jsx } from '@emotion/react/jsx-runtime' for files using emotion
-                    jsxImportSource: '@emotion/react',
-                  }),
-                ],
-              }),
-              compilerOptions: {
-                // set jsx pragma to jsx or alias which is from the @emotion/react package to enable css property in jsx component
-                jsxFactory: 'jsx',
-              },
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+            getCustomTransformers: () => ({
+              before: [
+                createEmotionPlugin({
+                  // <------------------- here
+                  sourcemap: true,
+                  autoLabel: true,
+                  labelFormat: '[local]',
+                  // if the jsxFactory is set, should we auto insert the import statement
+                  autoInject: true,
+                  // set for react@17 new jsx runtime
+                  // only effect if `autoInject` is true
+                  // set it in createEmotionPlugin options rather than in `tsconfig.json` will generate more optimized codes:
+                  // import { jsx } from 'react/jsx-runtime' for files not using emotion
+                  // import { jsx } from '@emotion/react/jsx-runtime' for files using emotion
+                  jsxImportSource: '@emotion/react',
+                }),
+              ],
+            }),
+            compilerOptions: {
+              // set jsx pragma to jsx or alias which is from the @emotion/react package to enable css property in jsx component
+              jsxFactory: 'jsx',
             },
-            exclude: /node_modules/,
           },
-        ],
+        },
       },
       {
         test: /\.(png|jpg|gif)$/,
