9f788aeca1da98e16194525fe5c0b0a6f2cda379
mango906 <minkb01@gmail.com>
2022-01-31 02:16:13 +0900

    코드 오너 테스트

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -3,6 +3,8 @@ import { useRecoilState } from 'recoil';
 import { countState } from '@/atoms';
 
 const Count = () => {
+  // 코드 오너 테스트
+
   const [count, setCount] = useRecoilState(countState);
   return (
     <div>
