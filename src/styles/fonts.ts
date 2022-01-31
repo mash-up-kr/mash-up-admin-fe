f878e49218a41974026018e7be3f99b58fb9adea
mango906 <minkb01@gmail.com>
2022-01-31 21:08:31 +0900

    Refactor: fonts 네이밍 변경
    snake-case에서 ko, en 분리하고 snake-case 안쓰도록 변경

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,76 +1,80 @@
 import { css } from '@emotion/react';
 
 export const fonts = {
-  kr_46_bold: css`
-    font-weight: 700;
-    font-size: 4.6rem;
-    line-height: 1.5;
-  `,
-  kr_22_bold: css`
-    font-weight: 700;
-    font-size: 2.2rem;
-    line-height: 1.5;
-  `,
-  kr_18_bold: css`
-    font-weight: 700;
-    font-size: 1.8rem;
-    line-height: 1.5;
-  `,
-  kr_16_medium: css`
-    font-weight: 500;
-    font-size: 1.6rem;
-    line-height: 1.5;
-  `,
-  kr_15_medium: css`
-    font-weight: 500;
-    font-size: 1.5rem;
-    line-height: 1.5;
-  `,
-  kr_15_regular: css`
-    font-weight: 400;
-    font-size: 1.5rem;
-    line-height: 1.5;
-  `,
-  kr_14_medium: css`
-    font-weight: 500;
-    font-size: 1.4rem;
-    line-height: 1.5;
-  `,
-  kr_14_regular: css`
-    font-weight: 400;
-    font-size: 1.4rem;
-    line-height: 1.5;
-  `,
-  kr_13_medium: css`
-    font-weight: 500;
-    font-size: 1.3rem;
-    line-height: 1.5;
-  `,
-  en_46_extrabold: css`
-    font-weight: 800;
-    font-size: 4.6rem;
-    line-height: 5.6rem;
-  `,
-  en_26_extrabold: css`
-    font-weight: 800;
-    font-size: 2.6rem;
-    line-height: 3.2rem;
-  `,
-  en_20_extrabold: css`
-    font-weight: 800;
-    font-size: 2rem;
-    line-height: 2.5rem;
-  `,
-  en_18_extrabold: css`
-    font-weight: 800;
-    font-size: 1.8rem;
-    line-height: 2.2rem;
-  `,
-  en_15_extrabold: css`
-    font-weight: 800;
-    font-size: 1.5rem;
-    line-height: 1.8rem;
-  `,
+  kr: {
+    bold46: css`
+      font-weight: 700;
+      font-size: 4.6rem;
+      line-height: 1.5;
+    `,
+    bold22: css`
+      font-weight: 700;
+      font-size: 2.2rem;
+      line-height: 1.5;
+    `,
+    bold18: css`
+      font-weight: 700;
+      font-size: 1.8rem;
+      line-height: 1.5;
+    `,
+    medium16: css`
+      font-weight: 500;
+      font-size: 1.6rem;
+      line-height: 1.5;
+    `,
+    medium15: css`
+      font-weight: 500;
+      font-size: 1.5rem;
+      line-height: 1.5;
+    `,
+    regular15: css`
+      font-weight: 400;
+      font-size: 1.5rem;
+      line-height: 1.5;
+    `,
+    medium14: css`
+      font-weight: 500;
+      font-size: 1.4rem;
+      line-height: 1.5;
+    `,
+    regular14: css`
+      font-weight: 400;
+      font-size: 1.4rem;
+      line-height: 1.5;
+    `,
+    medium13: css`
+      font-weight: 500;
+      font-size: 1.3rem;
+      line-height: 1.5;
+    `,
+  },
+  en: {
+    extrabold46: css`
+      font-weight: 800;
+      font-size: 4.6rem;
+      line-height: 5.6rem;
+    `,
+    extrabold26: css`
+      font-weight: 800;
+      font-size: 2.6rem;
+      line-height: 3.2rem;
+    `,
+    extrabold20: css`
+      font-weight: 800;
+      font-size: 2rem;
+      line-height: 2.5rem;
+    `,
+    extrabold18: css`
+      font-weight: 800;
+      font-size: 1.8rem;
+      line-height: 2.2rem;
+    `,
+    extrabold15: css`
+      font-weight: 800;
+      font-size: 1.5rem;
+      line-height: 1.8rem;
+    `,
+  },
 } as const;
 
 export type FontTheme = typeof fonts;
