4b1a997a4dc2003ee41262fe18b6c04880003ab8
mango906 <minkb01@gmail.com>
2022-01-31 21:10:52 +0900

    Fix: 영어 fonts들 line-height normal로 변경

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -52,27 +52,27 @@ export const fonts = {
     extrabold46: css`
       font-weight: 800;
       font-size: 4.6rem;
-      line-height: 5.6rem;
+      line-height: normal;
     `,
     extrabold26: css`
       font-weight: 800;
       font-size: 2.6rem;
-      line-height: 3.2rem;
+      line-height: normal;
     `,
     extrabold20: css`
       font-weight: 800;
       font-size: 2rem;
-      line-height: 2.5rem;
+      line-height: normal;
     `,
     extrabold18: css`
       font-weight: 800;
       font-size: 1.8rem;
-      line-height: 2.2rem;
+      line-height: normal;
     `,
     extrabold15: css`
       font-weight: 800;
       font-size: 1.5rem;
-      line-height: 1.8rem;
+      line-height: normal;
     `,
   },
 } as const;
