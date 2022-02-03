d99097c6525af93141c2809f878c96ef43719d30
mango906 <minkb01@gmail.com>
2022-02-03 19:56:45 +0900

    Env: storybook 데코레이터 추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/react';
+import { Global, ThemeProvider } from '@emotion/react';
+import { RecoilRoot } from 'recoil';
+import { theme, resetCss } from '@/styles';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +12,12 @@ export const parameters = {
     },
   },
 }
+
+addDecorator((Story) => (
+  <RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <Global styles={resetCss} />
+      <Story />
+    </ThemeProvider>
+  </RecoilRoot>
+));
