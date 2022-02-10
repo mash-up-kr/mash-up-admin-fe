2b9999e2ef99d255222772fe9514e196df14c221
dididy <dev.yongjaelee@gmail.com>
2022-02-10 21:56:47 +0900

    Feat: ToggelButton 컴포넌트를 구현한다

---
+++ src/components/common/ToggleButton/ToggleButton.styled.ts
@@ -0,0 +1,51 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { ToggleButtonProps } from './ToggleButton.component';
+
+export const ToggleButtonLabel = styled.label<Pick<ToggleButtonProps, 'disabled'>>`
+  ${({ disabled }) => css`
+    ${disabled && 'pointer-events: none; opacity: 0.5;'}
+    position: relative;
+    display: inline-block;
+    width: 4rem;
+    height: 2.4rem;
+  `}
+`;
+
+export const ToggleButtonInput = styled.input`
+  width: 0;
+  height: 0;
+  opacity: 0;
+`;
+
+export const ToggleButtonSlider = styled.span`
+  ${({ theme }) => css`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: #f6f6f6;
+    border-radius: 2.45rem;
+    cursor: pointer;
+    transition: 0.4s;
+    &::before {
+      position: absolute;
+      bottom: 0.2rem;
+      left: 0.2rem;
+      width: 2rem;
+      height: 2rem;
+      background-color: ${theme.colors.gray40};
+      border-radius: 50%;
+      transition: 0.4s;
+      content: '';
+    }
+    input:checked + & {
+      background-color: ${theme.colors.purple70};
+    }
+    input:checked + &::before {
+      background-color: ${theme.colors.white};
+      transform: translateX(1.6rem);
+    }
+  `}
+`;
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as Input } from './Input/Input';
 export { default as Button } from './Button/Button.component';
 export { default as ModalWrapper } from './ModalWrapper/ModalWrapper.component';
+export { default as ToggleButton } from './ToggleButton/ToggleButton.component';
