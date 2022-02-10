536fcb6ed563511d074c6a2881f807968efb3e59
mango906 <minkb01@gmail.com>
2022-02-11 01:34:09 +0900

    Feat: Textarea 컴포넌트 추가

---
+++ src/components/common/Textarea/Textarea.component.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as Styled from './Textarea.styled';
+
+export interface TextareaProps extends React.InputHTMLAttributes<HTMLTextAreaElement> {
+  id: string;
+  label: string;
+  errorMessage?: string;
+}
+
+const Textarea = ({ id, required, label, errorMessage, ...restProps }: TextareaProps) => {
+  return (
+    <Styled.TextareaWrapper>
+      <Styled.TextareaLabel htmlFor={id}>
+        <span>{label}</span>
+        {required && <Styled.RequiredDot />}
+      </Styled.TextareaLabel>
+      <Styled.Textarea {...restProps} />
+      {errorMessage && <Styled.TextareaErrorMessage>{errorMessage}</Styled.TextareaErrorMessage>}
+    </Styled.TextareaWrapper>
+  );
+};
+
+export default Textarea;
+
+Textarea.defaultProps = {
+  errorMessage: '',
+};
---
+++ src/components/common/Textarea/Textarea.styled.ts
@@ -0,0 +1,65 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+export const TextareaWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TextareaLabel = styled.label`
+  ${({ theme }) => css`
+    ${theme.fonts.medium15}
+    display: flex;
+    align-items: center;
+    margin-bottom: 0.6rem;
+    color: ${theme.colors.gray70};
+  `}
+`;
+
+export const RequiredDot = styled.span`
+  width: 0.6rem;
+  height: 0.6rem;
+  margin-left: 0.6rem;
+  background-color: #eb6963;
+  border-radius: 50%;
+`;
+
+export const Textarea = styled.textarea`
+  ${({ theme }) => css`
+    ${theme.fonts.regular15}
+
+    min-height: 20rem;
+    padding: 1.2rem 1.4rem;
+    background-color: ${theme.colors.white};
+    border: 1px solid ${theme.colors.gray30};
+    border-radius: 1.2rem;
+    outline: none;
+    resize: none;
+
+    &::placeholder {
+      color: ${theme.colors.gray50};
+    }
+
+    &:hover {
+      border: 0.1rem solid ${theme.colors.purple40};
+    }
+
+    &:focus {
+      border: 0.1rem solid ${theme.colors.purple70};
+    }
+
+    &:disabled {
+      background-color: ${theme.colors.gray5};
+      border: 0.1rem solid ${theme.colors.gray30};
+    }
+  `}
+`;
+
+export const TextareaErrorMessage = styled.span`
+  ${({ theme }) => css`
+    ${theme.fonts.regular15};
+
+    margin-top: 0.6rem;
+    color: ${theme.colors.gray60};
+  `}
+`;
