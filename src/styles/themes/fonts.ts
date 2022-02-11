ceb23bb9988c66e8158a036c0766b28b9bdadf17
dididy <dev.yongjaelee@gmail.com>
2022-02-12 04:00:17 +0900

    Env: emotion theme.fonts.bold24를 추가한다

--- src/styles/themes/fonts.ts
+++ src/styles/themes/fonts.ts
@@ -6,6 +6,11 @@ export const fonts = {
     font-size: 4.6rem;
     line-height: 1.5;
   `,
+  bold24: css`
+    font-weight: 700;
+    font-size: 2.4rem;
+    line-height: 1.5;
+  `,
   bold22: css`
     font-weight: 700;
     font-size: 2.2rem;
