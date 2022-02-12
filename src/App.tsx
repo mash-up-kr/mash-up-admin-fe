ac32a58d0c3e9b7c7613a4df87a087e2df010867
mango906 <minkb01@gmail.com>
2022-02-13 03:19:11 +0900

    Feat: Layout 전체적으로 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { Routes, Route } from 'react-router-dom';
 import { Global, ThemeProvider } from '@emotion/react';
 import { theme, globalStyles } from '@/styles';
 
+import { Layout } from '@/components';
 import Count from '@/components/Count';
 
 const App = () => (
   <>
     <Global styles={globalStyles} />
     <ThemeProvider theme={theme}>
-      <Routes>
-        <Route path="/" element={<Count />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Count />} />
+        </Routes>
+      </Layout>
     </ThemeProvider>
   </>
 );
