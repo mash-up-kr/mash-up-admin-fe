de58f34ff22db13115b1fbdb5b039d7eb437d0f3
dididy <dev.yongjaelee@gmail.com>
2022-02-15 02:58:43 +0900

    Refactor: Label 컴포넌트를 theme.label로 리팩토링 한다

--- src/components/common/Label/Label.styled.ts
+++ src/components/common/Label/Label.styled.ts
@@ -1,21 +1,20 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { ApplicationConfirmationStatusType, ApplicationResultStatusType } from './Label.component';
 
 interface StyledLabelProps {
-  backgroundColor: string;
-  color: string;
+  text: ApplicationConfirmationStatusType | ApplicationResultStatusType;
 }
 
 export const LabelWrapper = styled.div<StyledLabelProps>`
-  ${({ theme, color, backgroundColor }) => css`
+  ${({ theme, text }) => css`
     ${theme.fonts.medium13}
+    ${theme.label[text]}
     display: flex;
     align-items: center;
     justify-content: center;
     width: 8.6rem;
     height: 2.8rem;
-    color: ${color};
-    background-color: ${backgroundColor};
     border-radius: 0.8rem;
     cursor: default;
   `}
