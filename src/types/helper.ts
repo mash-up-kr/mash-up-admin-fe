7d401b678d8458bf7706cc5455208d2c0311f8e3
minsour <parkminsoo0128@gmail.com>
2022-02-16 04:32:44 +0900

    Feat: NestedKeyOf 헬퍼 타입 추가

--- src/types/helper.ts
+++ src/types/helper.ts
@@ -1,2 +1,8 @@
 export type KeyOf<T> = keyof T;
 export type ValueOf<T> = T[keyof T];
+
+export type NestedKeyOf<T extends object> = {
+  [Key in keyof T & (string | number)]: T[Key] extends object
+    ? `${Key}` | `${Key}.${NestedKeyOf<T[Key]>}`
+    : `${Key}`;
+}[keyof T & (string | number)];
