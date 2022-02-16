f48c13d4276386dfac559ca1f5b8032407f1cfae
dididy <dev.yongjaelee@gmail.com>
2022-02-17 00:37:26 +0900

    Feat: pathname에 따라 배경 색을 white, gray30 으로 토글되도록 처리한다

--- src/components/common/Layout/Layout.component.tsx
+++ src/components/common/Layout/Layout.component.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useMemo } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from '@/components';
 import * as Styled from './Layout.styled';
 
 const Layout = () => {
+  const { pathname } = useLocation();
+
+  const isBackgroundGray = useMemo(() => ['/login'].some((each) => each === pathname), [pathname]);
+
   return (
     <>
       <Header />
-      <Styled.Main>
-        <Outlet />
+      <Styled.Main isBackgroundGray={isBackgroundGray}>
+        <section>
+          <Outlet />
+        </section>
       </Styled.Main>
     </>
   );
--- src/components/common/Layout/Layout.styled.ts
+++ src/components/common/Layout/Layout.styled.ts
@@ -1,8 +1,19 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { HEADER_HEIGHT } from '@/constants';
 
-export const Main = styled.main`
-  max-width: 120rem;
-  min-height: calc(100vh - ${HEADER_HEIGHT});
-  margin: 0 auto;
+interface StyledMainProps {
+  isBackgroundGray: boolean;
+}
+
+export const Main = styled.main<StyledMainProps>`
+  ${({ isBackgroundGray, theme }) => css`
+    background-color: ${isBackgroundGray ? theme.colors.gray30 : theme.colors.white};
+
+    & > section {
+      max-width: 120rem;
+      min-height: calc(100vh - ${HEADER_HEIGHT});
+      margin: 0 auto;
+    }
+  `}
 `;
