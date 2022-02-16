9a1e6a1d369962c49e732eac90c194c428c68351
mango906 <minkb01@gmail.com>
2022-02-17 00:11:33 +0900

    Feat: Layout에 기본 배경색 들어가도록 추가

--- src/components/common/Layout/Layout.styled.ts
+++ src/components/common/Layout/Layout.styled.ts
@@ -1,7 +1,14 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 import { HEADER_HEIGHT } from '@/constants';
 
 export const Main = styled.main`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.gray10};
+  `}
+`;
+
+export const Content = styled.div`
   max-width: 120rem;
   min-height: calc(100vh - ${HEADER_HEIGHT});
   margin: 0 auto;
