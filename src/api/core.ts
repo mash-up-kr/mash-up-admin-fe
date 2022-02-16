953b9fbfea1fc5b235c14739788a1919db918183
dididy <dev.yongjaelee@gmail.com>
2022-02-17 03:51:09 +0900

    Feat: Axios를 편하게 사용하기 위한 util 함수를 추가한다

---
+++ src/api/core.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosInstance, AxiosRequestConfig, Method } from 'axios';
+import { ACCESS_TOKEN, HTTP_METHODS } from '@/constants';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: `${process.env.BASE_URL}/api/v1`,
+  timeout: 10000,
+  headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
+});
+
+axiosInstance.defaults.headers.common.Authorization =
+  localStorage.getItem(ACCESS_TOKEN) || process.env.DUMMY_TOKEN || '';
+
+const createApiMethod =
+  (_axiosInstance: AxiosInstance, methodType: Method) =>
+  // TODO:(용재) any 처리하기
+  (config: AxiosRequestConfig): Promise<any> => {
+    _axiosInstance.interceptors.response.use((response) => {
+      if (!response.data) return response;
+      return response.data.data;
+    });
+
+    return _axiosInstance({
+      ...config,
+      method: methodType,
+    });
+  };
+
+export default {
+  get: createApiMethod(axiosInstance, HTTP_METHODS.GET),
+  post: createApiMethod(axiosInstance, HTTP_METHODS.POST),
+  patch: createApiMethod(axiosInstance, HTTP_METHODS.PATCH),
+  put: createApiMethod(axiosInstance, HTTP_METHODS.PUT),
+  delete: createApiMethod(axiosInstance, HTTP_METHODS.DELETE),
+};
--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,3 +1,10 @@
+import { AxiosError } from 'axios';
+
 export const neverExpected = (value: never) => {
   throw new Error(`Unexpected value : ${value}`);
+};
+
+export const throwAPIErrorMessage = (error: AxiosError) => {
+  if (!error.response) throw error;
+  throw error.response.data.message;
 };
