64f02ae09b3a1c0e8d3b4127d4b25244db12de28
dididy <dev.yongjaelee@gmail.com>
2022-02-18 03:25:29 +0900

    Feat: RequiredAuth 컴포넌트로 로그인 여부에 따라 특정 경로로 리다이렉트 되도록 처리한다

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,81 @@
-import React from 'react';
+import React, { ReactNode, useEffect, useMemo } from 'react';
+import { Routes, Route, Navigate, NavigateProps } from 'react-router-dom';
+
 import { Global, ThemeProvider } from '@emotion/react';
-import { Route, Routes } from 'react-router-dom';
-import { theme, globalStyles } from '@/styles';
+import { useRecoilState } from 'recoil';
+import { theme, globalStyles } from './styles';
 
-import { ModalViewer, Layout } from '@/components';
-import LoginPage from '@/pages/LoginPage/LoginPage.page';
+import { ModalViewer, Layout } from './components';
+import LoginPage from './pages/LoginPage/LoginPage.page';
+import { $me } from './store';
+import { handleGetMyInfo } from './api/login';
+import { ACCESS_TOKEN } from './constants';
 
-const App = () => (
-  <>
-    <Global styles={globalStyles} />
-    <ThemeProvider theme={theme}>
-      <ModalViewer />
-      <Routes>
-        <Route path="/" element={<Layout />} />
-        {/* // TODO:(용재) path 객체 사용하도록 변경 */}
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </ThemeProvider>
-  </>
-);
+interface RequiredAuthProps extends Partial<NavigateProps> {
+  children: ReactNode;
+  isAuth: boolean;
+}
+
+// TODO:(용재) to 기본값 path 객체 이용하도록 변경
+const RequiredAuth = ({ children, isAuth, to = '/login', ...restProps }: RequiredAuthProps) => {
+  if (isAuth) {
+    return <Navigate {...restProps} to={to} />;
+  }
+
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <>{children}</>;
+};
+
+const App = () => {
+  const [me, setMe] = useRecoilState($me);
+
+  useEffect(() => {
+    (async () => {
+      const isToken = !!localStorage.getItem(ACCESS_TOKEN) ?? null;
+      if (isToken) {
+        try {
+          const data = await handleGetMyInfo();
+          setMe(data);
+        } catch (e) {
+          setMe({});
+        }
+      }
+    })();
+  }, [setMe]);
+
+  const isAuth = useMemo(() => Object.keys(me).length !== 0, [me]);
+
+  return (
+    <>
+      <Global styles={globalStyles} />
+      <ThemeProvider theme={theme}>
+        <ModalViewer />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* // TODO:(용재) 테스트용 - 추후 수정 */}
+            <Route
+              path="/application"
+              element={
+                <RequiredAuth isAuth={isAuth}>
+                  <div>test</div>
+                </RequiredAuth>
+              }
+            />
+          </Route>
+          {/* // TODO:(용재) path 객체 사용하도록 변경 */}
+          <Route
+            path="/login"
+            // TODO:(용재) 테스트용 - 추후 수정
+            element={
+              <RequiredAuth isAuth={!isAuth} to="/application">
+                <LoginPage />
+              </RequiredAuth>
+            }
+          />
+        </Routes>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
