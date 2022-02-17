1f78278a68b68afe1376307efd2b91cdb7c079ea
dididy <dev.yongjaelee@gmail.com>
2022-02-18 03:31:54 +0900

    Feat: response 받은 유저 정보를 토대로 헤더의 UserProfile을 반영하도록 한다

--- src/components/common/Header/Header.component.tsx
+++ src/components/common/Header/Header.component.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
 import * as Styled from './Header.styled';
 
 import Logo from '@/assets/svg/logo-admin-272.svg';
@@ -7,11 +8,17 @@ import {
   NavigationItem,
   NavigationSize,
 } from '@/components/common/Navigation/Navigation.component';
-import { Team, Role } from '@/components/common/UserProfile/UserProfile.component';
+import {
+  Team,
+  Role,
+  TeamType,
+  RoleType,
+} from '@/components/common/UserProfile/UserProfile.component';
 
 import { colors } from '@/styles';
 
 import { Navigation, UserProfile } from '@/components';
+import { $me } from '@/store';
 
 // TODO: (@mango90): router 변경해주기
 const navigationItems: NavigationItem[] = [
@@ -30,6 +37,17 @@ const navigationItems: NavigationItem[] = [
 ];
 
 const Header = () => {
+  const { data } = useRecoilValue($me);
+  // TODO:(용재) 더 좋은 방법 생각해보기
+  const [team, role] = useMemo(() => {
+    const pos = data?.adminMember?.position?.split('_');
+    if (pos?.length === 2) {
+      return pos;
+    }
+
+    return [Team.branding, Role.subLeader];
+  }, [data]);
+
   return (
     <Styled.HeaderContainer>
       <Styled.HeaderContainerInner>
@@ -43,7 +61,7 @@ const Header = () => {
           inActiveColor={colors.gray70}
           showBottomBorder={false}
         />
-        <UserProfile team={Team.branding} role={Role.subLeader} />
+        <UserProfile team={team as TeamType} role={role as RoleType} />
       </Styled.HeaderContainerInner>
     </Styled.HeaderContainer>
   );
