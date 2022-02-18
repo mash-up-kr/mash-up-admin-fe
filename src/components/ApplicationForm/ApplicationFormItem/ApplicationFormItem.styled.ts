e3125c45208e6c127ef287e7d6bda16d940c3320
mango906 <minkb01@gmail.com>
2022-02-19 06:00:13 +0900

    Fix: fields 폴더명 변경

--- src/components/ApplicationForm/ApplicationFormItem/ApplicationFormItem.styled.ts
+++ src/components/ApplicationForm/ApplicationFormItem/ApplicationFormItem.styled.ts
@@ -1,6 +1,6 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
-import { InputField } from '../../Field';
+import { InputField } from '../../fields';
 
 export const ApplicationFormItemContainer = styled.div`
   padding: 2.4rem 3.2rem;
