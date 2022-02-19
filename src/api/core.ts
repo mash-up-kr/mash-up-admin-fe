81b79daec9c8cafbaeb84fda795cfb5eb6155f2d
dididy <dev.yongjaelee@gmail.com>
2022-02-20 00:49:14 +0900

    Refactor: 불필요한 주석을 제거한다

--- src/api/core.ts
+++ src/api/core.ts
@@ -45,7 +45,6 @@ const handleError = (error: unknown) => {
 
 const createApiMethod =
   (_axiosInstance: AxiosInstance, methodType: Method) =>
-  // TODO:(용재) any 처리하기
   (config: AxiosRequestConfig): Promise<any> =>
     _axiosInstance({ ...handleRequest(config), method: methodType })
       .then(handleResponse)
