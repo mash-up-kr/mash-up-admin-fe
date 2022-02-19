0d2f2324e921a714e3c257a3954606cb8fedabfc
dididy <dev.yongjaelee@gmail.com>
2022-02-20 04:22:41 +0900

    Refactor: 연월일 / 시간 각각 유틸함수를 분리한다

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,8 +1,10 @@
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 
-export const convertToFormatDate = (rawDate: string, isTime = true) => {
-  return dayjs(rawDate)
-    .locale('ko')
-    .format(`YYYY년 M월 D일(ddd)${isTime ? ' 오후 h시 M분' : ''}`);
+export const convertToFormatDate = (rawDate: string) => {
+  return dayjs(rawDate).locale('ko').format(`YYYY년 M월 D일(ddd)`);
+};
+
+export const convertToFormatTime = (rawDate: string) => {
+  return dayjs(rawDate).locale('ko').format('a hh시 mm분');
 };
