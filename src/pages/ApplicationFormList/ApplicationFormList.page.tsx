8be8d8af8b1c0947e03fe16c9f8f696b9385578e
minsour <parkminsoo0128@gmail.com>
2022-02-19 20:30:58 +0900

    Feat: application api에 params 적용

--- src/pages/ApplicationFormList/ApplicationFormList.page.tsx
+++ src/pages/ApplicationFormList/ApplicationFormList.page.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilStateLoadable } from 'recoil';
+import dayjs from 'dayjs';
 import { usePagination } from '@/hooks';
 import { Pagination, Table } from '@/components';
 import { ApplicationFormResponse } from '@/types';
@@ -7,42 +8,57 @@ import { TableColumn } from '@/components/common/Table/Table.component';
 import { $applicationForms } from '@/store';
 
 const ApplicationFormList = () => {
-  const { pageOptions, handleChangePage, handleChangeSize } = usePagination(550);
-
   const columns: TableColumn<ApplicationFormResponse>[] = [
-    // {
-    //   title: '이름',
-    //   accessor: 'name',
-    //   renderCustomCell: (cellValue) => <div style={{ color: 'red' }}>{cellValue} asdasd</div>,
-    // },
     {
       title: '플랫폼',
       accessor: 'team.name',
+      widthRatio: '10%',
     },
     {
       title: '지원서 설문지 문서명',
       accessor: 'name',
+      widthRatio: '25%',
     },
     {
       title: '작성자',
       accessor: 'createdBy',
+      widthRatio: '10%',
     },
     {
       title: '작성일시',
       accessor: 'createdAt',
+      widthRatio: '20%',
+      renderCustomCell: (cellValue) => (
+        <span>{dayjs(cellValue as string).format('YYYY년 M월 DD일 오후 h시 m분')}</span>
+      ),
     },
     {
       title: '수정일시',
       accessor: 'updatedAt',
+      widthRatio: '20%',
+      renderCustomCell: (cellValue) => (
+        <span>{dayjs(cellValue as string).format('YYYY년 M월 DD일 오후 h시 m분')}</span>
+      ),
     },
     {
       title: '미리보기',
-      renderCustomCell: () => <div>미리보기</div>,
+      renderCustomCell: () => <div>icon</div>,
+      widthRatio: '5%',
     },
   ];
 
-  const [{ state, contents }] = useRecoilStateLoadable($applicationForms({}));
-  console.log(contents);
+  const [totalCount, setTotalCount] = useState(1);
+  const { pageOptions, handleChangePage, handleChangeSize } = usePagination(totalCount);
+  const [{ state, contents }] = useRecoilStateLoadable(
+    $applicationForms({
+      page: pageOptions.currentPage - 1,
+      size: pageOptions.pagingSize,
+    }),
+  );
+
+  useEffect(() => {
+    setTotalCount(contents.length);
+  }, [contents]);
 
   return (
     <div>
