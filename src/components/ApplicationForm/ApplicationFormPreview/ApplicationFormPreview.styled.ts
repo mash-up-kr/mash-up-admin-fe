c01f8f05f2ce87a3bd33f91debf44b2b6d5aee2a
mango906 <minkb01@gmail.com>
2022-03-15 19:03:45 +0900

    Feat: 지원설문지 생년월일, 거주지, 소속 필드 추가

--- src/components/ApplicationForm/ApplicationFormPreview/ApplicationFormPreview.styled.ts
+++ src/components/ApplicationForm/ApplicationFormPreview/ApplicationFormPreview.styled.ts
@@ -56,16 +56,6 @@ export const MaxContentLength = styled.span`
   `}
 `;
 
-export const Description = styled.span`
-  ${({ theme }) => css`
-    ${theme.fonts.regular15};
-
-    display: inline-block;
-    margin-top: 0.6rem;
-    color: ${theme.colors.gray60};
-  `}
-`;
-
 export const PrivacyCheckBox = styled(Checkbox)`
   margin-top: 3.6rem;
 `;
