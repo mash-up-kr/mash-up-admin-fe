668c7ce417e8fc52861755660e7a2699408fe2e9
minsour <parkminsoo0128@gmail.com>
2022-03-18 06:11:28 +0900

    Feat: useDirty hooks 추가

---
+++ src/hooks/useDirty.ts
@@ -0,0 +1,15 @@
+import { useState, useMemo } from 'react';
+
+const useDirty = (chance?: number) => {
+  const [count, setCount] = useState(0);
+
+  const makeDirty = () => setCount((prev) => prev + 1);
+  const isDirty = useMemo(() => count > chance || 0, [count, chance]);
+
+  return {
+    makeDirty,
+    isDirty,
+  };
+};
+
+export default useDirty;
