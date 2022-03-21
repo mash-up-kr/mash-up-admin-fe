eb9746deb8527886a2edb9015699b8228f1137a1
minsour <parkminsoo0128@gmail.com>
2022-03-21 09:20:02 +0900

    Feat: SmsSendDetailListModalDialog에 Table 추가

--- src/components/modal/SmsSendDetailListModalDialog/SmsSendDetailListModalDialog.component.tsx
+++ src/components/modal/SmsSendDetailListModalDialog/SmsSendDetailListModalDialog.component.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRecoilCallback } from 'recoil';
-import { ModalWrapper } from '@/components';
+import { ModalWrapper, Pagination, Table } from '@/components';
 import { $modalByStorage, ModalKey } from '@/store';
+import { SORT_TYPE } from '@/constants';
+import { usePagination } from '@/hooks';
+import * as Styled from './SmsSendDetailListModalDialog.styled';
 
-const SmsSendDetailListModalDialog = () => {
+const columns: TableColumn<ApplicationResponse>[] = [
+  {
+    title: '이름',
+    accessor: 'applicant.name',
+    idAccessor: 'applicationId',
+    widthRatio: '30%',
+  },
+  {
+    title: '전화번호',
+    accessor: 'applicant.phoneNumber',
+    widthRatio: '40%',
+  },
+  {
+    title: '지원플랫폼',
+    accessor: 'team.name',
+    widthRatio: '30%',
+  },
+];
+
+export interface SmsSendDetailListModalDialogProps {
+  selectedApplications: ApplicationResponse[];
+}
+
+const SmsSendDetailListModalDialog = ({
+  selectedApplications,
+}: SmsSendDetailListModalDialogProps) => {
   const handleRemoveCurrentModal = useRecoilCallback(({ set }) => () => {
     set($modalByStorage(ModalKey.smsSendDetailListModalDialog), {
       key: ModalKey.smsSendDetailListModalDialog,
@@ -11,7 +39,7 @@ const SmsSendDetailListModalDialog = () => {
     });
   });
 
-  const props = {
+  const modalProps = {
     heading: '발송인원 상세 리스트',
     footer: {
       cancelButton: {
@@ -22,8 +50,43 @@ const SmsSendDetailListModalDialog = () => {
     isContentScroll: false,
   };
 
-  // TODO: 테이블 추가
-  return <ModalWrapper {...props} />;
+  const { pageOptions, handleChangePage } = usePagination({
+    totalCount: 80, // selectedApplications.length,
+    pagingSize: 10,
+    pageButtonsSize: 7,
+    usingSearchParams: false,
+  });
+
+  const [sortTypes, setSortTypes] = useState<SortType<ApplicationResponse>[]>([
+    { accessor: 'applicant.name', type: SORT_TYPE.DEFAULT },
+    { accessor: 'team.name', type: SORT_TYPE.DEFAULT },
+  ]);
+
+  return (
+    <ModalWrapper {...modalProps}>
+      <Styled.TableWrapper>
+        <Table
+          prefix="application-sms"
+          width={60}
+          maxHeight={57}
+          columns={columns}
+          rows={selectedApplications}
+          supportBar={{
+            totalCount: selectedApplications.length,
+            totalSummaryText: '총 발송인원',
+          }}
+          sortOptions={{
+            sortTypes,
+            disableMultiSort: true,
+            handleSortColumn: (_sortTypes) => {
+              setSortTypes(_sortTypes);
+            },
+          }}
+          pagination={<Pagination pageOptions={pageOptions} handleChangePage={handleChangePage} />}
+        />
+      </Styled.TableWrapper>
+    </ModalWrapper>
+  );
 };
 
 export default SmsSendDetailListModalDialog;
