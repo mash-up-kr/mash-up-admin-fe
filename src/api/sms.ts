b8847ac332df7d8d0a84fe6365453f4e18229975
minsour <parkminsoo0128@gmail.com>
2022-03-28 11:31:27 +0900

    Feat: sms 관련 api 추가

--- src/api/sms.ts
+++ src/api/sms.ts
@@ -1,6 +1,19 @@
 import http from '@/api/core';
 import { BaseResponse } from '@/types';
-import { SmsRequest, SmsResponse } from '@/types/dto/sms';
+import { SmsByIdRequest, SmsRequest, SmsResponse } from '@/types/dto/sms';
+
+export const getSmsById = ({
+  notificationId,
+}: SmsByIdRequest): Promise<BaseResponse<SmsResponse>> =>
+  http.get({
+    url: `/notifications/${notificationId}`,
+  });
+
+export const getSmsSendingList = (params: SmsRequest): Promise<BaseResponse<SmsResponse[]>> =>
+  http.get({
+    url: '/notifications',
+    params,
+  });
 
 export const postSmsSend = ({
   applicantIds,
