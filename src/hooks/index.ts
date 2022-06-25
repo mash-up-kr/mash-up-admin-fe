d70d5626cf6262b7059062bb602473dd25e8fb08
mango906 <minkb01@gmail.com>
2022-06-25 21:01:18 +0900

    Feat: usePrompt 훅 추가

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -10,3 +10,4 @@ export { default as useBottomCTA } from './useBottomCTA';
 export { default as useWindowScroll } from './useWindowScroll';
 export { default as useConvertTextToLink } from './useConvertTextToLink';
 export { default as useHistory } from './useHistory';
+export { default as usePrompt } from './usePrompt';
