d78aef98b51af9011381da83c7be7b9b666d3ca6
mango906 <minkb01@gmail.com>
2022-08-20 19:43:42 +0900

    Feat: useHistory 훅에 쿼리스트링도 함꼐 저장하는 인자 추가

--- src/hooks/useHistory.ts
+++ src/hooks/useHistory.ts
@@ -2,15 +2,20 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 type HistoryLocationFromState = { from: string } | undefined;
 
-const useHistory = () => {
+const useHistory = (clearQueryString = true) => {
   const navigate = useNavigate();
   const location = useLocation();
+  const { search } = location ?? {};
   const { from } = (location.state as HistoryLocationFromState) ?? {};
 
   const handleNavigate = (to: string) => {
+    const currentPath = location.pathname;
+    const currentPathWithQueryString = `${location.pathname}${search}`;
+    const fromPath = clearQueryString ? currentPath : currentPathWithQueryString;
+
     navigate(to, {
       state: {
-        from: location.pathname,
+        from: fromPath,
       },
     });
   };
