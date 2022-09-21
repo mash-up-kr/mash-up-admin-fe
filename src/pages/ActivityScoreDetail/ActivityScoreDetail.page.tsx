44cea68de4c7795813be21518d27c96cff954dd9
mango906 <minkb01@gmail.com>
2022-09-22 01:39:33 +0900

    Feat: 활동점수 상세 모달 추가

--- src/pages/ActivityScoreDetail/ActivityScoreDetail.page.tsx
+++ src/pages/ActivityScoreDetail/ActivityScoreDetail.page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BackButton, Button, Table } from '@/components';
 import * as Styled from './ActivityScoreDetail.styled';
-import { useHistory } from '@/hooks';
+import { useHistory, useToggleState } from '@/hooks';
 import { PATH } from '@/constants';
 import {
+  ActivityScoreModalDialog,
   Icon,
   PersonalInfoCard,
   ScoreCard,
@@ -25,44 +26,6 @@ interface ScoreHistory {
   totalScore: number;
 }
 
-const columns: TableColumn<ScoreHistory>[] = [
-  {
-    title: '-',
-    widthRatio: '7%',
-    accessor: 'type',
-    renderCustomCell: (cellValue) => (
-      <Styled.IconWrapper>
-        <Icon type={cellValue as ValueOf<typeof ScoreType>} />
-      </Styled.IconWrapper>
-    ),
-  },
-  {
-    title: '제목',
-    widthRatio: '23%',
-    accessor: 'title',
-  },
-  {
-    title: '세미나 정보',
-    widthRatio: '23%',
-    accessor: 'seminar',
-  },
-  {
-    title: '등록 일시',
-    widthRatio: '23%',
-    accessor: 'createdAt',
-  },
-  {
-    title: '점수',
-    widthRatio: '12%',
-    accessor: 'score',
-  },
-  {
-    title: '총 활동 점수',
-    widthRatio: '12%',
-    accessor: 'totalScore',
-  },
-];
-
 const rows: ScoreHistory[] = Object.values(ScoreType).map((type) => ({
   type,
   title: ScoreTitle[type],
@@ -74,23 +37,70 @@ const rows: ScoreHistory[] = Object.values(ScoreType).map((type) => ({
 
 const ActivityScoreDetail = () => {
   const { handleGoBack } = useHistory();
+  const [isScoreAddModalOpened, toggleScoreAddModalOpened] = useToggleState(false);
+
+  const columns: TableColumn<ScoreHistory>[] = [
+    {
+      title: '-',
+      widthRatio: '7%',
+      accessor: 'type',
+      renderCustomCell: (cellValue) => (
+        <Styled.IconWrapper>
+          <Icon type={cellValue as ValueOf<typeof ScoreType>} />
+        </Styled.IconWrapper>
+      ),
+    },
+    {
+      title: '제목',
+      widthRatio: '23%',
+      accessor: 'title',
+      renderCustomCell: (cellValue) => (
+        <Styled.ActivityTitle onClick={toggleScoreAddModalOpened}>
+          {cellValue as string}
+        </Styled.ActivityTitle>
+      ),
+    },
+    {
+      title: '세미나 정보',
+      widthRatio: '23%',
+      accessor: 'seminar',
+    },
+    {
+      title: '등록 일시',
+      widthRatio: '23%',
+      accessor: 'createdAt',
+    },
+    {
+      title: '점수',
+      widthRatio: '12%',
+      accessor: 'score',
+    },
+    {
+      title: '총 활동 점수',
+      widthRatio: '12%',
+      accessor: 'totalScore',
+    },
+  ];
 
   return (
-    <Styled.ActivityScoreDetailPage>
-      <BackButton label="목록 돌아가기" onClick={() => handleGoBack(PATH.ACTIVITY_SCORE)} />
-      <Styled.Headline>활동점수</Styled.Headline>
-      <Styled.Row>
-        <PersonalInfoCard />
-        <ScoreCard />
-      </Styled.Row>
-      <Styled.Content>
-        <Styled.ContentHeader>
-          <h3>활동점수 히스토리</h3>
-          <Button shape={ButtonShape.defaultLine} label="점수 추가" Icon={Plus} />
-        </Styled.ContentHeader>
-        <Table prefix="score-history" columns={columns} rows={rows} supportBar={{}} />
-      </Styled.Content>
-    </Styled.ActivityScoreDetailPage>
+    <>
+      <Styled.ActivityScoreDetailPage>
+        <BackButton label="목록 돌아가기" onClick={() => handleGoBack(PATH.ACTIVITY_SCORE)} />
+        <Styled.Headline>활동점수</Styled.Headline>
+        <Styled.Row>
+          <PersonalInfoCard />
+          <ScoreCard />
+        </Styled.Row>
+        <Styled.Content>
+          <Styled.ContentHeader>
+            <h3>활동점수 히스토리</h3>
+            <Button shape={ButtonShape.defaultLine} label="점수 추가" Icon={Plus} />
+          </Styled.ContentHeader>
+          <Table prefix="score-history" columns={columns} rows={rows} supportBar={{}} />
+        </Styled.Content>
+      </Styled.ActivityScoreDetailPage>
+      {isScoreAddModalOpened && <ActivityScoreModalDialog onClose={toggleScoreAddModalOpened} />}
+    </>
   );
 };
 
