77a0c1a60e2bd838349705b65498377b59d341af
mango906 <minkb01@gmail.com>
2022-09-30 00:37:08 +0900

    Fix: 활동점수 상세 path 수정
    url에 기수정보도 포함되어야 해서 수정

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -10,7 +10,7 @@ export const PATH = {
   APPLICATION_FORM_CREATE: '/application-form/create',
   APPLICATION_FORM_UPDATE: '/application-form/update/:id',
   ACTIVITY_SCORE: '/activity-score',
-  ACTIVITY_SCORE_DETAIL: '/activity-score/:id',
+  ACTIVITY_SCORE_DETAIL: '/activity-score/:generationNumber/:memberId',
   NOT_FOUND: '/404',
   FORBIDDEN: '/403',
 } as const;
