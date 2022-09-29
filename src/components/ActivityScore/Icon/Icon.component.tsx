64980a6a8832adff83936cca7b44cabfa26c8c10
mango906 <minkb01@gmail.com>
2022-09-30 01:44:01 +0900

    Fix: ScoreType 서버 명세와 동일하게 변경

--- src/components/ActivityScore/Icon/Icon.component.tsx
+++ src/components/ActivityScore/Icon/Icon.component.tsx
@@ -6,7 +6,6 @@ import Absent from '@/assets/svg/absent-40.svg';
 import Presentation from '@/assets/svg/presentation-40.svg';
 import ProjectSuccess from '@/assets/svg/project-success-40.svg';
 import ProjectFail from '@/assets/svg/project-fail-40.svg';
-import ContentsWrite from '@/assets/svg/contents-write-40.svg';
 import Leader from '@/assets/svg/leader-40.svg';
 import SubLeader from '@/assets/svg/sub-leader-40.svg';
 import Staff from '@/assets/svg/staff-40.svg';
@@ -20,16 +19,15 @@ const scoreIcon = {
   [ScoreType.ATTENDANCE]: <Attendance />,
   [ScoreType.LATE]: <LATE />,
   [ScoreType.ABSENT]: <Absent />,
-  [ScoreType.PRESENTATION]: <Presentation />,
-  [ScoreType.PROJECT_SUCCESS]: <ProjectSuccess />,
-  [ScoreType.PROJECT_FAIL]: <ProjectFail />,
-  [ScoreType.CONTENTS_WRITE]: <ContentsWrite />,
-  [ScoreType.LEADER]: <Leader />,
-  [ScoreType.SUB_LEADER]: <SubLeader />,
-  [ScoreType.STAFF]: <Staff />,
+  [ScoreType.SEMINAR_PRESENTATION]: <Presentation />,
+  [ScoreType.DEPLOY_SUCCESS]: <ProjectSuccess />,
+  [ScoreType.DEPLOY_FAILURE]: <ProjectFail />,
+  [ScoreType.MASHUP_LEADER]: <Leader />,
+  [ScoreType.MASHUP_SUBLEADER]: <SubLeader />,
+  [ScoreType.MASHUP_STAFF]: <Staff />,
   [ScoreType.PROJECT_LEADER]: <ProjectLeader />,
-  [ScoreType.PROJECT_SUB_LEADER]: <ProjectSubLeader />,
-  [ScoreType.PREPARE]: <Prepare />,
+  [ScoreType.PROJECT_SUBLEADER]: <ProjectSubLeader />,
+  [ScoreType.HACKATHON_COMMITTEE]: <Prepare />,
 };
 
 interface IconProps {
