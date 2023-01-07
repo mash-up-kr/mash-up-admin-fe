bcbd3eecc2e8389df04f488e55bc686275ece586
sanoopark <sanoopark@gmail.com>
2023-01-08 00:04:18 +0900

    Refactor: Recoil Selector의 SMS API를 이메일로 변경
    - `제출` 유형의 이메일 항목을 제외하는 로직 포함

--- src/store/sms.ts
+++ src/store/sms.ts
@@ -1,29 +1,28 @@
-import {
-  BaseResponse,
-  SmsResponse,
-  SmsSendingListResponse,
-  SmsSendingListRequest,
-  SmsByIdRequest,
-} from '@/types';
+import { BaseResponse, EmailResponse, EmailSendingListRequest, EmailByIdRequest } from '@/types';
 import * as api from '@/api';
 import { selectorFamilyWithRefresher } from './recoil';
 
-export const $smsSendingDetail = selectorFamilyWithRefresher<SmsResponse, SmsByIdRequest>({
-  key: 'smsSendingDetail',
+const excludeSubmitEmail = (data: BaseResponse<EmailResponse[]>) => {
+  const excludedEmailData = data.data.filter(({ type }) => type !== 'SUBMIT');
+  return { ...data, data: excludedEmailData };
+};
+
+export const $emailSendingDetail = selectorFamilyWithRefresher<EmailResponse, EmailByIdRequest>({
+  key: 'emailSendingDetail',
   get: (params) => async () => {
-    const { data } = await api.getSmsById(params);
+    const { data } = await api.getEmailById(params);
 
     return data;
   },
 });
 
-export const $smsSendingList = selectorFamilyWithRefresher<
-  BaseResponse<SmsSendingListResponse[]>,
-  SmsSendingListRequest
+export const $emailSendingList = selectorFamilyWithRefresher<
+  BaseResponse<EmailResponse[]>,
+  EmailSendingListRequest
 >({
-  key: 'smsSendingList',
+  key: 'emailSendingList',
   get: (params) => async () => {
-    const data = await api.getSmsSendingList(params);
-    return data;
+    const data = await api.getEmailSendingList(params);
+    return excludeSubmitEmail(data);
   },
 });
