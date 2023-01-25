44d683faa3f85bd759b52991cc1123bcf0557c49
mango906 <minkb01@gmail.com>
2023-01-25 15:25:19 +0900

    feat: 스케쥴 등록 api 추가

--- src/api/schedule.ts
+++ src/api/schedule.ts
@@ -1,9 +1,13 @@
 import { BaseResponse } from '@/types';
 import http from '@/api/core';
-import { ScheduleRequest, ScheduleResponse } from '@/types/dto/schedule';
+import { ScheduleRequest, ScheduleResponse, CreateScheduleRequest } from '@/types/dto';
 
 export const getSchedules = (params: ScheduleRequest): Promise<BaseResponse<ScheduleResponse[]>> =>
   http.get({
     url: '/schedules',
     params,
   });
+
+export const createSchedule = (
+  data: CreateScheduleRequest,
+): Promise<BaseResponse<ScheduleResponse>> => http.post({ url: '/schedules', data });
