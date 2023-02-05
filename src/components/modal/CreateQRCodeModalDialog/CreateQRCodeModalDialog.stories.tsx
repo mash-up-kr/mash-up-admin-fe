89a2ec7469435e1297f57cb1dda0e2077ad9d891
sanoopark <sanoopark@gmail.com>
2023-02-06 00:38:01 +0900

    Refactor: Storybook의 Props를 추가하다

--- src/components/modal/CreateQRCodeModalDialog/CreateQRCodeModalDialog.stories.tsx
+++ src/components/modal/CreateQRCodeModalDialog/CreateQRCodeModalDialog.stories.tsx
@@ -7,6 +7,13 @@ export default {
   component: CreateQRCodeModalDialog,
 } as ComponentMeta<typeof CreateQRCodeModalDialog>;
 
-const Template: ComponentStory<typeof CreateQRCodeModalDialog> = () => <CreateQRCodeModalDialog />;
+const Template: ComponentStory<typeof CreateQRCodeModalDialog> = () => (
+  <CreateQRCodeModalDialog
+    scheduleId={100038}
+    eventId={100101}
+    sessionStartedAt="2023-01-27T18:00"
+    sessionEndedAt="2023-01-27T21:00"
+  />
+);
 
 export const createQRCodeModalDialog = Template.bind({});
--- src/components/modal/DisplayQRCodeModalDialog/DisplayQRCodeModalDIalog.stories.tsx
+++ src/components/modal/DisplayQRCodeModalDialog/DisplayQRCodeModalDIalog.stories.tsx
@@ -8,7 +8,7 @@ export default {
 } as ComponentMeta<typeof DisplayQRCodeModalDialog>;
 
 const Template: ComponentStory<typeof DisplayQRCodeModalDialog> = () => (
-  <DisplayQRCodeModalDialog />
+  <DisplayQRCodeModalDialog qrCodeUrl="https://chart.apis.google.com/chart?cht=qr&chs=300x300&chl=8ac39" />
 );
 
 export const displayQRCodeModalDialog = Template.bind({});
