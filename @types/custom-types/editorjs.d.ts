3b78e803881f389210a82728d5e19ce52bd6105b
sanoopark <sanoopark@gmail.com>
2023-04-29 16:18:02 +0900

    Build: editorjs 의존성을 추가하다

---
+++ @types/custom-types/editorjs.d.ts
@@ -0,0 +1,8 @@
+declare module '@editorjs/list';
+declare module '@editorjs/checklist';
+declare module '@editorjs/table';
+declare module '@editorjs/marker';
+declare module '@editorjs/underline';
+declare module '@editorjs/underline';
+declare module '@editorjs/text-variant-tune';
+declare module 'editorjs-drag-drop';
