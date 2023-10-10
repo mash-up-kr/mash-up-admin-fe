a655e8f959c097fc9753aabec2e24e839389016d
sanoopark <sanoopark@gmail.com>
2023-10-10 20:02:32 +0900

    Refactor: 중복제거를 위한 리팩토링을 하다

--- src/components/common/Editor/Editor.component.tsx
+++ src/components/common/Editor/Editor.component.tsx
@@ -3,7 +3,7 @@ import EditorJS, { OutputData } from '@editorjs/editorjs';
 import DragDrop from 'editorjs-drag-drop';
 import { Blocker } from '@/components';
 import i18n from './i18n';
-import { getDefaultEditorData } from '@/utils';
+import { getDefaultEditorData, setLocalStorageData } from '@/utils';
 import tools from './tools';
 
 interface EditorProps {
@@ -13,25 +13,27 @@ interface EditorProps {
 
 const Editor = ({ id, savedData }: EditorProps) => {
   const editorRef = useRef<EditorJS>();
-  const [editorData, setEditorData] = useState<OutputData>(savedData);
+  const [editorData, setEditorData] = useState<OutputData | undefined>(savedData);
   const [editorReady, setEditorReady] = useState(false);
+
+  const getEditorContent = async (): Promise<OutputData | undefined> => {
+    return editorRef.current?.saver.save();
+  };
 
   const initEditor = () => {
     const editor = new EditorJS({
       holder: id,
       data: editorData,
-      onReady: async () => {
+      onReady: () => {
         editorRef.current = editor;
         // eslint-disable-next-line no-new
         new DragDrop(editor);
         setEditorReady(true);
-        const content = (await editorRef.current?.saver.save()) as OutputData;
-        localStorage.setItem(id, JSON.stringify(content));
       },
       onChange: async () => {
-        const content = (await editorRef.current?.saver.save()) as OutputData;
-        localStorage.setItem(id, JSON.stringify(content));
-        setEditorData(content);
+        const editorContent = await getEditorContent();
+        setLocalStorageData(id, editorContent);
+        setEditorData(editorContent);
       },
       autofocus: true,
       // @ts-expect-error: third party plugin
@@ -76,7 +78,7 @@ const Editor = ({ id, savedData }: EditorProps) => {
   return (
     <>
       <div id={id} />;
-      <Blocker isBlocking={editorData.blocks?.length !== 0} />
+      <Blocker isBlocking={editorData?.blocks?.length !== 0} />
     </>
   );
 };
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,3 +1,7 @@
 export const getLocalStorageData = (key: string) => {
   return JSON.parse(localStorage.getItem(key) ?? '{}');
 };
+
+export const setLocalStorageData = (key: string, data: any) => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
